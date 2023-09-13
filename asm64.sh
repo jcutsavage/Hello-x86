11d08ed0894f444df71de1c36a5bb69ff10951ed
John Cutsavage <jcutsavage@gmail.com>
2023-09-12 21:51:36 -0400

    Fixed typo in bash script name. Updated build script to use nasm.

--- asm64.sh
+++ asm64.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-# Simple assemble/link script from Ed Jorgensen, Ph.D UNLV
+# Simple assemble/link script orginally from Ed Jorgensen, Ph.D UNLV
+# Modified to use nasm assembler
 
-if [ -z $1]; then
+if [ -z $1 ]; then
     echo "Usage: ./asm64.sh <asmMainFile> (no extension)"
     exit
 fi
 
 # Verify no extensions were entered
 
-if [! -e "$1.asm"]; then
+if [ ! -e "$1.asm" ]; then
     echo "Error, $1.asm not found."
     echo "Note, do not enter file extensions."
     exit
@@ -16,5 +17,5 @@ fi
 
 # Compile, assemble, and link
 
-yasm -Worphan-labels -g dwarf2 -f elf64 $1.asm -l $1.lst
+nasm -f elf64 -g -F DWARF $1.asm -l $1.lst
 ld -g -o $1 $1.o
